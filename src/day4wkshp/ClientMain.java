48b3f249eec28b985fbbd276099cdf68b9100273
Yong_Ler_Xin <yonglerxin@gmail.com>
2024-10-10 19:08:12 +0800

    Update:File Server Transfer

--- src/day4wkshp/ClientMain.java
+++ src/day4wkshp/ClientMain.java
@@ -1,2 +1,65 @@
 package day4wkshp;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ClientMain{
+
+    public static void main(String[] args)throws IOException {
+        if (args.length <= 0){
+            System.err.println("No file Specified");
+            System.exit(-1);
+        }
+
+        int port = 3000;
+
+        File file = new File(args[0]);
+
+        Socket sock = new Socket("localhost",port);
+
+        OutputStream os = sock.getOutputStream();
+        BufferedOutputStream bos = new BufferedOutputStream(os);
+        DataOutputStream dos = new DataOutputStream(bos);
+
+        FileInputStream fis = new FileInputStream(file);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+
+        String fileName = file.getName();
+        long filesize = file.length();
+
+        dos.writeUTF(fileName);
+        dos.writeLong(filesize);
+
+        int readBytes = 0;
+        int sendbytes = 0;
+        byte[] buff = new byte[4*1024];
+
+        while (((readBytes = bis.read(buff)) > 0 )){
+            sendbytes += readBytes;
+            dos.write(buff,0,readBytes);
+            System.out.printf("Send %d of %d\n", sendbytes, filesize);
+
+        }
+        // Close the file
+        bis.close();
+        fis.close();
+
+        // Flush connection
+        dos.flush();
+        bos.flush();
+        os.flush();
+
+        // Close connection
+        dos.close();
+        bos.close();
+        os.close();
+        sock.close();
+
+
+
+    }
+
+
+
+}
--- src/day4wkshp/FileServer.java
+++ src/day4wkshp/FileServer.java
@@ -1,5 +1,5 @@
 package day4wkshp;
-//incomplete to review using profs code
+//modify for multiple files?
 
 import java.io.*;
 import java.net.*;
@@ -11,19 +11,50 @@ public class FileServer {
         public static void main(String[] args) throws IOException {
             int port = 3000;
 
-            if(args.length > 0 ){
-                port  =Integer.parseInt(args[0]);
-            }
+            // if(args.length > 0 ){
+            //     port  =Integer.parseInt(args[0]);
+            // }
             //create server port
             ServerSocket server = new ServerSocket(port);
 
+            while(true){
+            System.out.println("Waiting for connection...");
             Socket sock = server.accept();
 
-            OutputStream os = sock.getOutputStream();
-            BufferedOutputStream bos = new BufferedOutputStream(os);
-            DataOutputStream dos = new DataOutputStream(bos);
+            InputStream is = sock.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+            DataInputStream dis = new DataInputStream(bis);
+
+            String fileName = dis.readUTF();
+            long fileSize = dis.readLong();
+
+            FileOutputStream fos = new FileOutputStream(fileName);
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+            int bytesRead = 0;
+            int recvBytes = 0;
+            byte[] buff = new byte[4*1024];
+
+            while (recvBytes < fileSize){
+                bytesRead = dis.read(buff);
+                bos.write(buff,0,bytesRead);
+                recvBytes += bytesRead;
+                System.out.printf("Received %d of %d\n",recvBytes,fileSize);
 
 
+            }
+            // Close the new file
+            bos.flush();
+            fos.close();
+
+            // Close the socket
+            dis.close();
+            bis.close();
+            is.close();
+            sock.close();
+            
+
+            }
 
 
             
